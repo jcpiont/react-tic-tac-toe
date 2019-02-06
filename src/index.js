5c90ad962b4741aaf13aa61e6fafc90368a6aee0
jcpiont <jcpiont@gmail.com>
2019-02-06 04:44:04 +0000

    Feat: Create board refactored to loop
    fixes #3

--- src/index.js
+++ src/index.js
@@ -93,26 +93,21 @@ class Board extends React.Component {
     );
   }
 
+  createSquares() {
+    var i, j;
+    let rows = [];
+    for (i = 0; i < 3; i++) {
+      let squares = [];
+      for (j = 0; j < 3; j++) {
+        squares.push(this.renderSquare(i * 3 + j));
+      }
+      rows.push(<div className="board-row">{squares}</div>);
+    }
+    return rows;
+  }
+
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{this.createSquares()}</div>;
   }
 }
 
