85aa6c7706a991a727ba405c8806b0b911711b7e
jcpiont <jcpiont@gmail.com>
2023-03-26 10:38:08 -0400

    Updates

---
+++ precache-manifest.0983ca6016ac603b968ed0915f1cc907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe1530ec2e2330ae4514",
+    "url": "/react-tic-tac-toe/static/css/main.20dd8424.chunk.css"
+  },
+  {
+    "revision": "fe1530ec2e2330ae4514",
+    "url": "/react-tic-tac-toe/static/js/main.fe1530ec.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/react-tic-tac-toe/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "87d698cc4d3f04a284f2",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.87d698cc.js"
+  },
+  {
+    "revision": "742383c4d57898c2f28f418c749d0c9c",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
